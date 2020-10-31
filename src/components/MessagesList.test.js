7bac2c253bcba9bbb726d16b7543aeef80ff9e74
Annora-Charming <harchebnikova99@gmail.com>
2020-11-01 01:11:17 +0700

    Update
    Added snapshot test to Message.test and inline snapshot test to MessagesList.test

--- src/components/MessagesList.test.js
+++ src/components/MessagesList.test.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import MessagesList from './MessagesList';
-test('Messages list', () => {
-    const messages = [
-        {nick: "TEST", message: "TEST"}
-    ];
-    const component = shallow(<MessagesList messages={messages}/>);
-    expect(component.find('Message')).toHaveLength(1);
+import React from "react";
+import { shallow } from "enzyme";
+import MessagesList from "./MessagesList";
+import renderer from "react-test-renderer";
+
+test("Messages list", () => {
+  const messages = [{ nick: "TEST", message: "TEST" }];
+  const component = shallow(<MessagesList messages={messages} />);
+  expect(component.find("Message")).toHaveLength(1);
+});
+
+test("inline snapshot test", () => {
+  const messages = [{ nick: "TEST", message: "TEST" }];
+  const component = renderer
+    .create(<MessagesList messages={messages} />)
+    .toJSON();
+  expect(component).toMatchInlineSnapshot(`
+    <div
+      id="messages_list"
+    >
+      <ul>
+        <li>
+          <b>
+            TEST
+            :
+          </b>
+          TEST
+        </li>
+      </ul>
+      ;
+    </div>
+  `);
 });
